cd8337b68928f38fa38990a8b78a14ffd7679763
Felipe T.S. Silva <felsilva@girolando.com.br>
2022-12-01 17:12:03 -0300

    Implementação de login

--- TasksTemplate/Fonte/app/src/main/java/com/devmasterteam/tasks/viewmodel/LoginViewModel.kt
+++ TasksTemplate/Fonte/app/src/main/java/com/devmasterteam/tasks/viewmodel/LoginViewModel.kt
@@ -2,6 +2,8 @@ package com.devmasterteam.tasks.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.devmasterteam.tasks.service.listener.APIlistener
 import com.devmasterteam.tasks.service.model.PersonModel
 import com.devmasterteam.tasks.service.repository.PersonRepository
@@ -9,17 +11,24 @@ import com.devmasterteam.tasks.service.repository.PersonRepository
 class LoginViewModel(application: Application) : AndroidViewModel(application) {
 
     private val personRepository = PersonRepository(application.applicationContext)
+
+    private  val _login = MutableLiveData<Boolean>()
+    val login: LiveData<Boolean> = _login
+
+    private  val _failure = MutableLiveData<String>()
+    val failure: LiveData<String> = _failure
+
+
     /**
      * Faz login usando API
      */
     fun doLogin(email: String, password: String){
         personRepository.login(email,password, object : APIlistener<PersonModel> {
             override fun onSuccess(result: PersonModel) {
-
+                _login.value = true
             }
-
             override fun onFailure(message: String) {
-
+                _failure.value = message
             }
 
 
